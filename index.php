ba6874af92abd9c34a2a5046d87752da6182a022
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-03 10:40:38 +0100

    Add currency and currencyRate objects

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php
 var_dump($_POST);
+
+// $euroCurrency = 1;
+// $americanDollars = 1.10;
+// $japansesYen = 156.20;
+// $angolanKwanza = 911.87;
+// $burundianFrancs = 3122.99;
+
+
+$currencies = (object) [
+    'EURO' => 'EURO',
+    'USD' => 'AMERICAN DOLLARS',
+    'JPY' => 'JAPANESE YEN',
+    'AOA' => 'ANGOLAN KWANZA',
+    'BIF' => 'Burundian Francs'
+];
+
+$currenciesRate = (object) [
+    'EURO' => 1,
+    'USD' => 1.09,
+    'JPY' => 155.97,
+    'AOA' => 910.03,
+    'BIF' => 3120.31
+];
+
+
+echo $currencies;
+
+
+function currencyChange($currency, $amount) {
+
+};
 ?>
 
 <!DOCTYPE html>
@@ -9,10 +40,26 @@ var_dump($_POST);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Currency exchange app</title>
 </head>
+<style>
+
+body {
+    background: gray;
+}
+
+main {
+    margin-top: 30px;
+}
+
+form {
+    display: flex;
+    flex-direction: row;
+    gap: 7px; 
+}
+</style>
 <body>
     <main>
-        <form action="" method="post">
-            <input type="text" name="name">
+        <form action="index.php" method="post">
+            <input type="number" name="amount">
             <br>
             <button>post</button>
         </form>
