4fa4bcdf7a26c1588f2f4f30bd8157a3f020be6d
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-03 09:41:31 +0100

    Create form and input wiht Post method

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-echo "Hello World!"
+var_dump($_POST);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Currency exchange app</title>
+</head>
+<body>
+    <main>
+        <form action="" method="post">
+            <input type="text" name="name">
+            <br>
+            <button>post</button>
+        </form>
+    </main>
+</body>
+</html>
